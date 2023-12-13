bb08c39b1e28e28d839336abe1bd824d105b1613
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-12-13 14:39:05 +0200

    adjust qr code

--- DecryptTheMessage/Program.cs
+++ DecryptTheMessage/Program.cs
@@ -9,12 +9,9 @@
                 - Eliminați caracterele "^" și "~" din text.
                 - Înlocuiți caracterele "{", "}", "(", ")" cu spații.
                 - Înlocuiți cifrele cu literele corespunzătoare.
-                    Hint 1: Pentru 0 inlocuiti cu "A", s.a.m.d
                 - Inversați ordinea caracterelor rămase în text.
                 - Înlocuiți caracterele cu literele corespunzătoare, utilizând cheia Constants.Key.
                   Cheia este folosita in cadrul unui algoritm de criptare cu substitutie, alfabetul suportat fiind specificat in Constants.Alphabet.
-                    Hint 2: Drept autor al tehnicii de criptare, este creditat imparatul roman Iulius Caesar (12 July 100 BC – 15 March 44 BC).
-                    Hint 3: Fiecare litera din cheie reprezinta de fapt un deplasament (shift), in functie de indexul ei din alfabet
                 - Afișați mesajul rezultat.
              */
 
