968ab503372d564b2432be121b0f44937034de26
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-12-13 19:58:17 +0200

    Share intermediary steps values

---
+++ DecryptTheMessage/IntermediaryStepsValues.cs
@@ -0,0 +1,14 @@
+﻿namespace DecryptTheMessage
+{
+    public static class IntermediaryStepsValues
+    {
+        /*
+
+After applying step (remove '^', '~'):                        >&%%S,Kf*10zc3n5,4QQ1<6NRL43y91je62<L,OMP!l91PPWRwM;2S4Zy?*#XU!hS%;eKQfL%>MN3K
+After applying step (replace "{", "}", "(", ")" with spaces): >&%%S,Kf*10zc3n5,4QQ1<6NRL43y91je62<L,OMP!l91PPWRwM;2S4Zy?*#XU!hS%;eKQfL%>MN3K
+After applying step (replace digits with letters):            >&%%S,Kf*BAzcDnF,EQQB<GNRLEDyJBjeGC<L,OMP!lJBPPWRwM;CSEZy?*#XU!hS%;eKQfL%>MNDK
+After applying step (reverse string):                         KDNM>%LfQKe;%Sh!UX#*?yZESC;MwRWPPBJl!PMO,L<CGejBJyDELRNG<BQQE,FnDczAB*fK,S%%&>
+
+         */
+    }
+}
